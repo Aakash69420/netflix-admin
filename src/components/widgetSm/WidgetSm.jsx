6e3013339c3a4563c575cb9bb0d793eb1a50eb66
Aakash69420 <aakashbhattacharjee73@gmail.com>
2023-04-26 11:42:15 +0530

    aniruudha ki

---
+++ src/components/widgetSm/WidgetSm.jsx
@@ -0,0 +1,52 @@
+import "./widgetSm.css";
+import { Visibility } from "@material-ui/icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function WidgetSm() {
+  const [newUsers, setNewUsers] = useState([]);
+
+  useEffect(() => {
+    const getNewUsers = async () => {
+      try {
+        const res = await axios.get("/users?new=true", {
+          headers: {
+            token:
+              "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY0MzkyMmIzY2E2NjVlMDExODZjOGExMiIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY4MjI0MTczMSwiZXhwIjoxNjgyNjczNzMxfQ.8KrFqrR-w5nQMVb3lb1AI0DjQbPP09fUuBcd_-1Kakk",
+          },
+        });
+        setNewUsers(res.data);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+    getNewUsers();
+  }, []);
+  
+  return (
+    <div className="widgetSm">
+      <span className="widgetSmTitle">New Join Members</span>
+      <ul className="widgetSmList">
+        {newUsers.map((user, i) => (
+          <li key={i} className="widgetSmListItem">
+            <img
+              src={
+                user.profilePic ||
+                "https://pbs.twimg.com/media/D8tCa48VsAA4lxn.jpg"
+              }
+              alt=""
+              className="widgetSmImg"
+            />
+            <div className="widgetSmUser">
+              <span className="widgetSmUsername">{user.username}</span>
+            </div>
+            <button className="widgetSmButton">
+              <Visibility className="widgetSmIcon" />
+              Display
+            </button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/pages/movie/Movie.jsx
@@ -0,0 +1,76 @@
+import { Link, useLocation } from "react-router-dom";
+import "./movie.css";
+import { Publish } from "@material-ui/icons";
+
+export default function Movie() {
+  const location = useLocation();
+  const { movie } = location.state;
+  const object = movie.reduce(
+    (obj, item) => Object.assign(obj, { ['_id']: item._id, ['img']: item.img, ['title']: item.title, ['year']: item.year, ['genre']: item.genre, ['limit']: item.limit, ['trailer']: item.trailer, ['video']: item.video }), {});
+    console.log(object)
+
+  return (
+    <div className="product">
+      <div className="productTitleContainer">
+        <h1 className="productTitle">Movie</h1>
+        <Link to="/newMovie">
+          <button className="productAddButton">Create</button>
+        </Link>
+      </div>
+          <div className="productTop">
+            <div className="productTopRight">
+              <div className="productInfoTop">
+                <img src={object.img} alt="" className="productInfoImg" />
+                <span className="productName">{object.title}</span>
+              </div>
+              <div className="productInfoBottom">
+                <div className="productInfoItem">
+                  <span className="productInfoKey">id:</span>
+                  <span className="productInfoValue">{object._id}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">genre:</span>
+                  <span className="productInfoValue">{object.genre}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">year:</span>
+                  <span className="productInfoValue">{object.year}</span>
+                </div>
+                <div className="productInfoItem">
+                  <span className="productInfoKey">limit:</span>
+                  <span className="productInfoValue">{object.limit}</span>
+                </div>
+              </div>
+            </div>
+          </div>
+      <div className="productBottom">
+        <form className="productForm">
+          <div className="productFormLeft">
+            <label>Movie Title</label>
+            <input type="text" defaultValue={object.title} />
+            <label>Year</label>
+            <input type="text" defaultValue={object.year} />
+            <label>Genre</label>
+            <input type="text" defaultValue={object.genre} />
+            <label>Limit</label>
+            <input type="text" defaultValue={object.limit} />
+            <label>Trailer</label>
+            <input type="file" />
+            <label>Video</label>
+            <input type="file" />
+          </div>
+          <div className="productFormRight">
+            <div className="productUpload">
+              <img src={object.img} alt="" className="productUploadImg" />
+              <label htmlFor="file">
+                <Publish />
+              </label>
+              <input type="file" id="file" style={{ display: "none" }} />
+            </div>
+            <button className="productButton">Update</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+}
